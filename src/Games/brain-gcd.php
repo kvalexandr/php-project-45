23aedae8f150302abc88763b06d3b6645029b7eb
Alexandr Kudryavtcev <k.v.alexandr@gmail.com>
2025-02-06 19:07:22 +0400

    fix gcd game

--- src/Games/brain-gcd.php
+++ src/Games/brain-gcd.php
@@ -2,15 +2,29 @@
 
 namespace BrainGames\Games\brain\gcd;
 
+function gcd(int $num1, int $num2): string
+{
+    $maxNum = max($num1, $num2);
+    $minNum = min($num1, $num2);
+
+    for ($i = $minNum; $i > 0; $i--) {
+        if ($maxNum % $i === 0 && $minNum % $i === 0) {
+            return (string)$i;
+        }
+    }
+
+    return '0';
+}
+
 function getQuestionAndAnswer(): array
 {
     $number1 = rand(1, 100);
     $number2 = rand(1, 100);
 
     $question = "{$number1} {$number2}";
-    $answer = gmp_gcd($number1, $number2);
+    $answer = gcd($number1, $number2);
 
-    return [$question, (string)$answer];
+    return [$question, $answer];
 }
 
 function getData()
